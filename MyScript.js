2cd12652e847149b1bdcb312e9e50374b9ee66cf
Ayman M.Ahmed <evilayman@gmail.com>
2017-11-29 18:34:30 +0200

    Added Bullet and Bullet Collision, Added Gamepad Support

--- MyScript.js
+++ MyScript.js
@@ -5,6 +5,11 @@ var speed = 200;
 var bulletSpeed = 500;
 var myBullets = [];
 
+var bulletsGroup,enimesGroup;
+
+//Pad Variables
+var padAimX,padAimY, pressFlagA = 0;
+
 function preload()
 {
 	game.load.image('player','assets/player.png');
@@ -20,52 +25,54 @@ function create()
 	game.scale.pageAlignVertically = true;
 	game.scale.refresh();
 	
+	//Create Groups
+	bulletsGroup = game.add.group();
+	enimesGroup = game.add.group();
+	
 	//Create Player
 	player = game.add.sprite(200, 200, 'player');
 	player.anchor.setTo(0.5, 0.5);
 	player.scale.setTo(0.5, 0.5);
 	game.physics.arcade.enable(player);
 	player.body.collideWorldBounds=true;
-	
+
 	//Create Arrow
 	arrow = game.add.sprite(200,200,'arrow');
 	arrow.anchor.setTo(0, 0.5);
 	arrow.scale.setTo(0.05, 0.05);
 	game.physics.arcade.enable(arrow);
 	
-	//Keyboard Controls
+	//Keyboard
 	cursors = game.input.keyboard.createCursorKeys();
 	upButton = game.input.keyboard.addKey(Phaser.Keyboard.W);
 	downButton = game.input.keyboard.addKey(Phaser.Keyboard.S);
 	leftButton = game.input.keyboard.addKey(Phaser.Keyboard.A);
 	rightButton = game.input.keyboard.addKey(Phaser.Keyboard.D);
 	
-
+	//Gamepad 
+	game.input.gamepad.start();
+	pad1 = game.input.gamepad.pad1;
 	
 }
 
 function update()
 {
-	movePlayer();
-	
-	//Control Arrow Movement
-	arrow.position.x = player.position.x;
-	arrow.position.y = player.position.y;
-	arrow.rotation = game.physics.arcade.angleToPointer(arrow);
-	
-	//Create Bullet on Click and Check Collison
-	game.input.onDown.add(Shoot, this);
+	if(player)
+	{
+		//Choose Between Keyboard or Gamepad
+		movePlayer();
+		//movePlayerPad();
+		
+		//Control Arrow Movement
+		arrow.position.x = player.position.x;
+		arrow.position.y = player.position.y;
+	}
+		
+	//Check Bullet Collison
 	for(let i = 0; i < myBullets.length; i++)
 	{
 		checkBulletCollison(myBullets[i]);
 	}
-
-}
-
-function Shoot() 
-{
-	var bullet = createBullet(50, 50);
-	myBullets.push(bullet);
 }
 
 function checkBulletCollison(myBullet)
@@ -83,15 +90,48 @@ function checkBulletCollison(myBullet)
 			}
 		}
 		//Check Player and Bullet Collision
-		game.physics.arcade.collide(myBullet[0],player,killplayer);
+		game.physics.arcade.collide(myBullet[0],player,bpCollision);
+
+		//Check Bullet and Bullet Collision
+		try
+		{
+			game.physics.arcade.collide(myBullet[0],bulletsGroup,bbCollision);
+		}
+		catch(err)
+		{
+			console.log(err.message);
+		}
+		
 	}
 }
-function killplayer(b)
+
+function bpCollision(b)
 {
 	player.destroy();
+	player = null;
 	arrow.destroy();
-	b.destroy();
+	arrow = null;
+	bulletSearchDestroy(b);
 }
+
+function bbCollision(b1,b2)
+{
+	bulletSearchDestroy(b1);
+	bulletSearchDestroy(b2);
+}
+
+function bulletSearchDestroy(bullet)
+{
+	for(let i = 0; i < myBullets.length; i++)
+	{
+		if(myBullets[i][0] == bullet)
+		{
+			myBullets[i][0].destroy();
+			myBullets[i][0] = null;
+		}
+	}
+}
+
 function createBullet(posX,posY)
 {
 	var bullet = game.add.sprite(posX,posY,'bullet');
@@ -100,10 +140,17 @@ function createBullet(posX,posY)
 	game.physics.arcade.enable(bullet);
 	bullet.body.collideWorldBounds = true;
 	bullet.body.bounce.setTo(1.0,1.0);
+	
 	//Use this to Move to Mouse Location
-	game.physics.arcade.moveToPointer(bullet, bulletSpeed);
+	//game.physics.arcade.moveToPointer(bullet, bulletSpeed);
+	
 	//Use this to Move to Player Location (Our Main Usage)
-	//game.physics.arcade.moveToObject(bullet, player, bulletSpeed);
+	game.physics.arcade.moveToObject(bullet, player, bulletSpeed);
+	
+	//Use this to Move to Position(X,Y)
+	//game.physics.arcade.moveToXY(bullet,200,100,bulletSpeed);
+	
+	bulletsGroup.add(bullet);
 	
 	var bulletArr = [bullet, 3];
 	return bulletArr;
@@ -113,7 +160,6 @@ function createBullet(posX,posY)
 function movePlayer()
 {
 	slowTime();
-	console.log(game.time.desiredFps);
 	player.body.velocity.x = 0;
 	player.body.velocity.y = 0;
 
@@ -137,21 +183,92 @@ function movePlayer()
 		player.body.velocity.x = speed;
 		resetTime();
 	}
+	
+	//Control Arrow Direction
+	arrow.rotation = game.physics.arcade.angleToPointer(arrow);
+	
+	//Create Bullet on Click
+	game.input.onDown.add(Shoot, this);
+}
+
+function Shoot() 
+{
+	if(player)
+		myBullets.push(createBullet(50, 50));
+}
+
+function movePlayerPad()
+{
+	slowTime();
+	player.body.velocity.x = 0;
+	player.body.velocity.y = 0;	
+
+	padAimX = player.position.x;
+	padAimY = player.position.y;
+	
+	if (pad1.isDown(Phaser.Gamepad.XBOX360_DPAD_LEFT) || pad1.axis(Phaser.Gamepad.XBOX360_STICK_LEFT_X) < -0.1)
+    {
+        player.body.velocity.x = -speed;
+		resetTime();
+		arrow.rotation = game.physics.arcade.angleToXY(arrow,padAimX,padAimY);
+    }
+    else if (pad1.isDown(Phaser.Gamepad.XBOX360_DPAD_RIGHT) || pad1.axis(Phaser.Gamepad.XBOX360_STICK_LEFT_X) > 0.1)
+    {
+        player.body.velocity.x = speed;
+		resetTime();
+		arrow.rotation = game.physics.arcade.angleToXY(arrow,padAimX,padAimY);
+    }
+    if (pad1.isDown(Phaser.Gamepad.XBOX360_DPAD_UP) || pad1.axis(Phaser.Gamepad.XBOX360_STICK_LEFT_Y) < -0.1)
+    {
+        player.body.velocity.y = -speed;
+		resetTime();
+		arrow.rotation = game.physics.arcade.angleToXY(arrow,padAimX,padAimY);
+    }
+    else if (pad1.isDown(Phaser.Gamepad.XBOX360_DPAD_DOWN) || pad1.axis(Phaser.Gamepad.XBOX360_STICK_LEFT_Y) > 0.1)
+    {
+        player.body.velocity.y = speed;
+		resetTime();
+		arrow.rotation = game.physics.arcade.angleToXY(arrow,padAimX,padAimY);
+    }
+
+	if (pad1.axis(Phaser.Gamepad.XBOX360_STICK_RIGHT_X) < -0.1)
+    {
+		padAimX = player.position.x - 1;
+		arrow.rotation = game.physics.arcade.angleToXY(arrow,padAimX,padAimY);
+    }
+    else if (pad1.axis(Phaser.Gamepad.XBOX360_STICK_RIGHT_X) > 0.1)
+    {
+		padAimX = player.position.x + 1;
+		arrow.rotation = game.physics.arcade.angleToXY(arrow,padAimX,padAimY);
+    }
+    if (pad1.axis(Phaser.Gamepad.XBOX360_STICK_RIGHT_Y) < -0.1)
+    {
+		padAimY = player.position.y - 1;
+		arrow.rotation = game.physics.arcade.angleToXY(arrow,padAimX,padAimY);
+    }
+    else if (pad1.axis(Phaser.Gamepad.XBOX360_STICK_RIGHT_Y) > 0.1)
+    {
+		padAimY = player.position.y + 1;
+		arrow.rotation = game.physics.arcade.angleToXY(arrow,padAimX,padAimY);
+    }
+	if (pad1.justPressed(Phaser.Gamepad.XBOX360_A))
+    {
+        pressFlagA = 1;
+    }
+	if (pad1.justReleased(Phaser.Gamepad.XBOX360_A) && pressFlagA)
+    {
+        Shoot();
+		pressFlagA = 0;
+    }
 }
 
 function slowTime()
 {
-	game.time.slowMotion = 4;
-	game.time.desiredFps = 240;
+	game.time.slowMotion = 6;
+	game.time.desiredFps = 360;
 }
 function resetTime()
 {
 	game.time.slowMotion = 1;
 	game.time.desiredFps = 60;
 }
-
-
-
-
-
-
