4bc14207e42518ab7b174d4fa89c63dff44e073e
DESKTOP-MPV28VQ\tasnim <tassneemm0@gmail.com>
2017-12-05 00:01:36 +0200

    xx

--- MyScript.js
+++ MyScript.js
@@ -1,4 +1,4 @@
-var game = new Phaser.Game(800, 600, Phaser.Physics.ARCADE, '', { preload: preload, create: create, update: update });
+var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 var player, arrow, cursors, upButton, downButton, leftButton, rightButton;
 var speed = 200;
 var myArc, relfectFlag = 0;
